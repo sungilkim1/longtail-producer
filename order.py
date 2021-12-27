3540692b24468666c65d7a8a0e20a11a6487a587
darwin <darwinkim@gmail.com>
2021-12-27 19:56:29 +0900

    confluent kafka

--- order.py
+++ order.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from kafka import KafkaProducer
+from confluent_kafka import Producer
+import socket
+
 import sqlite3
 import sys
 import json
@@ -11,6 +14,7 @@ import logging
 app 	= Flask(__name__)
 
 kafka_server = 'my-cluster-kafka-bootstrap.my-kafka-project:9092'
+confluent_kafka_server = 'kafka.confluent.svc.cluster.local:9071'
 
 @app.route('/')
 def hello():
@@ -33,6 +37,25 @@ def orderTransaction():
 	producer.send('order', value=params)
 	producer.flush()
 	return 'Order added to queue';
+
+@app.route('/corder')
+def orderTransaction():
+	logging.info('confluent order topic')
+	conf = {'bootstrap.servers': "host1:9092,host2:9092",
+			'client.id': socket.gethostname()}
+
+	params_dict = request.args.to_dict()
+	if len(params_dict) == 0:
+		return 'No Parameter'
+
+	params = {}
+	for key in params_dict.keys():
+		params[key] = request.args[key]
+
+	producer.produce('order', value=params)
+	producer.flush()
+	
+	return 'Confluent:Order added to queue';
 
 @app.route('/dispatch')
 def dispatchTransaction():
